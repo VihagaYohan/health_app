616e1bb24342b47df2771d7998405101de4ab4d0
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-11-30 07:49:36 +0530

    Added - bottom navigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Hi, Jane"),
+            Text("How are you feeling today?",
+                style: TextStyle(
+                    fontFamily: 'PlusJakartaSans',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12))
+          ],
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Ionicons.notifications_outline)),
+          IconButton(onPressed: () {}, icon: const Icon(Ionicons.search))
+        ],
+      ),
+      body: ListView(children: const []),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Ionicons.home_outline), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Ionicons.calendar_outline), label: 'Calendar'),
+            BottomNavigationBarItem(
+                icon: Icon(Ionicons.chatbubble_outline), label: "Chats"),
+            BottomNavigationBarItem(
+                icon: Icon(Ionicons.person_outline), label: "Profile")
+          ]),
+    );
   }
 }
